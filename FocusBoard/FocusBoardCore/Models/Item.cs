f5b2363bd695347d47747f30ea317166feae2595
Will Blackburn <harkole@gmail.com>
2019-01-29 20:28:36 +0000

    Added Data validation to the Model class for Item

--- FocusBoard/FocusBoardCore/Models/Item.cs
+++ FocusBoard/FocusBoardCore/Models/Item.cs
@@ -1,4 +1,5 @@
-﻿using System.Runtime.Serialization;
+﻿using System.ComponentModel.DataAnnotations;
+using System.Runtime.Serialization;
 
 namespace FocusBoardCore.Models
 {
@@ -7,14 +8,22 @@ namespace FocusBoardCore.Models
     {
         [DataMember(Name = "id")]
         public string Id { get; set; } = string.Empty;
+
         [DataMember(Name = "author")]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Author is required")]
         public string Author { get; set; } = string.Empty;
+
         [DataMember(Name = "hideAuthor")]
         public bool HideAuthor { get; set; } = false;
+
         [DataMember(Name = "title")]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Title is required")]
         public string Title { get; set; } = string.Empty;
+
         [DataMember(Name = "description")]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Description must not be blank")]
         public string Description { get; set; } = string.Empty;
+
         [DataMember(Name = "votes")]
         public int Votes { get; set; } = 0;
     }
