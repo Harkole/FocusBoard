9ac58e694ac7625064c87283b61b527750c5c608
Will Blackburn <harkole@gmail.com>
2019-02-05 21:26:02 +0000

    Added Model Validation rules

--- FocusBoard/FocusBoardCore/Models/Comment.cs
+++ FocusBoard/FocusBoardCore/Models/Comment.cs
@@ -1,4 +1,5 @@
-﻿using System.Runtime.Serialization;
+﻿using System.ComponentModel.DataAnnotations;
+using System.Runtime.Serialization;
 
 namespace FocusBoardCore.Models
 {
@@ -7,12 +8,19 @@ namespace FocusBoardCore.Models
     {
         [DataMember(Name = "id")]
         public string Id { get; set; } = string.Empty;
+
         [DataMember(Name = "authorId")]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Author Identity must be provided")]
         public string AuthorId { get; set; } = string.Empty;
+
         [DataMember(Name = "parentId")]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Parent Identity must be provided")]
         public string ParentId { get; set; } = string.Empty;
+
         [DataMember(Name = "comment")]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Comments must have a text value")]
         public string Value { get; set; } = string.Empty;
+
         [DataMember(Name = "votes")]
         public int Votes { get; set; } = 0;
     }
